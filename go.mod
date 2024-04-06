62d34e45b02c3971454b5f0306fb82c82deb535b
Kazuhiro Watanabe <ralirulego103@gmail.com>
2024-04-06 17:26:36 +0900

    Use statik

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/jackc/pgx/v5 v5.5.1
 	github.com/lib/pq v1.10.7
 	github.com/o1egl/paseto v1.0.0
+	github.com/rakyll/statik v0.1.7
 	github.com/spf13/viper v1.15.0
 	github.com/stretchr/testify v1.8.2
 	golang.org/x/crypto v0.9.0
--- main.go
+++ main.go
@@ -10,10 +10,12 @@ import (
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/legobrokkori/go-kubernetes-grpc_practice/api"
 	db "github.com/legobrokkori/go-kubernetes-grpc_practice/db/sqlc"
+	_ "github.com/legobrokkori/go-kubernetes-grpc_practice/doc/statik"
 	"github.com/legobrokkori/go-kubernetes-grpc_practice/gapi"
 	"github.com/legobrokkori/go-kubernetes-grpc_practice/pb"
 	"github.com/legobrokkori/go-kubernetes-grpc_practice/util"
 	_ "github.com/lib/pq"
+	"github.com/rakyll/statik/fs"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/encoding/protojson"
@@ -84,8 +86,13 @@ func runGatewayServer(config util.Config, store db.Store) {
 	mux := http.NewServeMux()
 	mux.Handle("/", grpcMux)
 
-	fs := http.FileServer(http.Dir("./doc/swagger"))
-	mux.Handle("/swagger/", http.StripPrefix("/swagger/", fs))
+	statikFS, err := fs.New()
+	if err != nil {
+		log.Fatal("cannot create statik fs: ", err)
+	}
+
+	swaggerHandler := http.StripPrefix("/swagger/", http.FileServer(statikFS))
+	mux.Handle("/swagger/", swaggerHandler)
 
 	listener, err := net.Listen("tcp", config.HttpServerAddress)
 	if err != nil {
